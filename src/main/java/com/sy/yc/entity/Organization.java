eabca6ab1f5d40cb0be06ca950d1237ac6bd3e71
gaowei <gaowei@gooagoo.com>
2019-09-03 09:44:52 +0800

    org

---
+++ src/main/java/com/sy/yc/entity/Organization.java
@@ -0,0 +1,16 @@
+package com.sy.yc.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName Organization
+ * @Description: TODO
+ * @Author Administrator
+ * @Date 2019/9/3
+ * @Version V1.0
+ **/
+@Data
+public class Organization {
+    private String id;
+    private String name;
+}
